e3ee377d199e59c896befbd5f217a3b4099085a4
Mathieu Gautier <mathieu.gautier@inra.fr>
2019-06-20 14:00:02 +0000

    version 1.1.0

--- R/is.pooldata.R
+++ R/is.pooldata.R
@@ -4,7 +4,7 @@
 #' @slot nsnp The number of SNPs
 #' @slot refallele.readcount A matrix (nsnp rows and npools columns) with read count data for the reference allele
 #' @slot readcoverage A matrix (nsnp rows and Npools columns) with overall read coverage
-#' @slot snp.info A matrix (nsnp rows and 4 columns) detailing for each SNP, the chromosome (or scaffold), the position, the reference allele and the alternative allele
+#' @slot snp.info A matrix (nsnp rows and 4 columns) detailing for each SNP, the chromosome (or scaffold), the position, allele 1 and allele 2
 #' @slot poolsizes A vector of length npools with the corresponding haploid pool sizes
 #' @slot poolnames A vector of length npools with the corresponding haploid pool names
 #' @seealso To generate pooldata object, see \code{\link{vcf2pooldata}}, \code{\link{popsync2pooldata}}, \code{\link{genobaypass2pooldata}} and \code{\link{genoselestim2pooldata}}
